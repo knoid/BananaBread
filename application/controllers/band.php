28527b7fbc31062f450268131bb7ea27b6bb4207
Ariel Barabas <ariel.baras@gmail.com>
2013-10-18 13:03:48 -0300

    remap

--- application/controllers/band.php
+++ application/controllers/band.php
@@ -7,7 +7,12 @@ class Band extends MY_Controller {
 		$this->load->model('band_model');
 		$this->load_view('band', array(
 			'band' => $this->band_model->get_by_id($band_id, TRUE)
-		);
+		));
+	}
+
+	function _remap($band_id)
+	{
+		$this->index($band_id);
 	}
 
 }

94c55adfc51d5ee0eb8848ec0c287c5432a8e4a7
Ariel Barabas <ariel.baras@gmail.com>
2013-10-18 02:08:56 -0300

    created events view

---
+++ application/controllers/events.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Events extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->model('event_model');
+
+		$this->load->view('events', array(
+			'events' => $this->event_model->find_near(-34.6033, -58.3817)
+		));
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
---
+++ application/views/layout.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+	<body>
+		<?php $this->view($tpl_view, $tpl_data) ?>
+	</body>
+</html>

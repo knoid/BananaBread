f4d9aed417869c4432b20ced5e8d0355418611f8
Ariel Barabas <ariel.baras@gmail.com>
2013-10-18 21:26:37 -0300

    band controller

--- application/config/routes.php
+++ application/config/routes.php
@@ -41,6 +41,8 @@
 $route['default_controller'] = "events";
 $route['404_override'] = '';
 
+$route['band/(:num)'] = 'band/show/$1';
+
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
--- application/controllers/band.php
+++ application/controllers/band.php
@@ -2,17 +2,42 @@
 
 class Band extends MY_Controller {
 
-	public function index($band_id)
+	public function show($band_id)
 	{
 		$this->load->model('band_model');
 		$this->load_view('band', array(
-			'band' => $this->band_model->get_by_id($band_id, TRUE)
+			'band' => $this->band_model->get_by_id($band_id, TRUE),
+			'edit' => count($this->session->userdata('band')) > 0
 		));
 	}
 
-	function _remap($band_id)
+	public function upload($to_service)
 	{
-		$this->index($band_id);
+		if ($to_service == 'youtube')
+		{
+			if (isset($_FILES['file']))
+			{
+				$tmp_name = tempnam('/tmp/', 'vfu');
+				if (move_uploaded_file($_FILES['file']['tmp_name'], $tmp_name))
+				{
+					$this->session->set_userdata('file_upload', array($tmp_name, $_FILES['file']['type']));
+				}
+			}
+
+			$this->load->model('auth_model');
+			list($token, $user) = $this->auth_model->login('google');
+			$band = $this->session->userdata('band');
+
+			$file_upload = $this->session->userdata('file_upload');
+			if ($file_upload)
+			{
+				$this->load->model('band_model');
+				$video_data = $this->band_model->add_youtube_upload($band, $token, $file_upload);
+				$this->session->set_userdata('file_upload', FALSE);
+			}
+
+			redirect('band/' . $band->band_id);
+		}
 	}
 
 }

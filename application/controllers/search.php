0802c23a30e1cbfe64e4c9157a116e4bc15f44b8
Ariel Barabas <ariel.baras@gmail.com>
2013-10-19 05:08:05 -0300

    tinysearch

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -5,6 +5,7 @@ class Search extends MY_Controller {
 	public function index()
 	{
 		$queries = array_filter(explode(' ', $this->input->get('query')));
+		$query_terms = implode(' ', $queries);
 
 		foreach ($queries as $term)
 			foreach (array('description', 'name') as $field)
@@ -12,9 +13,18 @@ class Search extends MY_Controller {
 
 		$res = $this->db->get('band');
 
+		$this->config->load('oauth2');
+		$ch = curl_init("http://tinysong.com/s/$query_terms?" . http_build_query(array(
+			'format' => 'json',
+			'key'    => $this->config->item('tinysong')
+		)));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$data = json_decode(curl_exec($ch));
+
 		$this->load_view('search', array(
-			'search_items' => $res->result(),
-			'query_terms' => implode(' ', $queries)
+			'our_items' => $res->result(),
+			'tiny_items' => $data,
+			'query_terms'  => $query_terms
 		));
 	}
 

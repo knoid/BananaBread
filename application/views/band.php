3d7e1bb3aad5f8da6e3b6143512cc77d96269fa0
Ariel Barabas <ariel.baras@gmail.com>
2013-10-19 01:22:05 -0300

    removed view

--- application/views/band.php
+++
@@ -1,9 +0,0 @@
-<?php echo $band->name ?>
-<img src="<?php echo $band->link ?>"/>
-YouTube
-<?php if ($edit): ?>
-	<form action="<?php echo site_url('band/upload/youtube') ?>" method="post" enctype="multipart/form-data">
-		<input type="file" name="file" id="youtube_upload">
-		<input type="submit">
-	</form>
-<?php endif ?>
